23515c7e067a4f1609ae967d90bd2fad1ef63e74
bc <martooo1936@gmail.com>
2019-11-16 19:15:19 +0100

    Concrete builder added

---
+++ WebShopPatterns/Builder/BasicAdBuilder.cs
@@ -0,0 +1,25 @@
+﻿namespace WebShopPatterns.Builder
+{
+    public class BasicAdBuilder : AdBuilder
+    {
+        public override void SetName()
+        {
+            _ad.Name = "";
+        }
+
+        public override void SetDescription()
+        {
+            _ad.Description = "";
+        }
+
+        public override void SetPrice()
+        {
+            _ad.Price = 1;
+        }
+
+        public override void SetRating()
+        {
+            _ad.Rating = 2;
+        }
+    }
+}
